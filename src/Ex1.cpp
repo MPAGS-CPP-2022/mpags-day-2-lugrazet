a4e85b77182c9df585a6c951da932ddae8589db3
Luke Grazette <Luke Grazette Luke.Grazette@cern.ch>
2022-10-21 09:21:17 +0000

    Updated Ex1 and removed bugs
    Specifically it would print out x between each character and not change
    the numerical values to the necessary strings. Now fixed.

--- src/Ex1.cpp
+++ src/Ex1.cpp
@@ -12,49 +12,51 @@ std::string transform_char(const char in_char)
     Transforms a number to it's word form 
     (i.e. 1 -> ONE)
     Capitalises an alpha char
+    returns string
     */
 
-    std::string out_char{'x'};
+    std::string out_char{};
     if (std::isalnum(in_char)) {
+        std::cout << (in_char) << "\n";
         switch (in_char) {
             case '0':
-                out_char += in_char;
+                out_char += "ZERO";
                 break;
 
             case '1':
-                out_char += in_char;
+                out_char += "ONE";
                 break;
 
             case '2':
-                out_char += in_char;
+                out_char += "TWO";
                 break;
 
             case '3':
-                out_char += in_char;
+                out_char += "THREE";
                 break;
 
             case '4':
-                out_char += in_char;
+                out_char += "FOUR";
                 break;
 
             case '5':
-                out_char += in_char;
+                out_char += "FIVE";
                 break;
 
             case '6':
-                out_char += in_char;
+                out_char += "SIX";
                 break;
 
             case '7':
-                out_char += in_char;
+                out_char += "SEVEN";
                 break;
 
             case '8':
-                out_char += in_char;
+                out_char += "EIGHT";
                 break;
 
             case '9':
-                out_char += in_char;
+                out_char += "NINE";
                 break;
             default:
                 out_char += std::toupper(in_char);
